9974c74f19093f1ab4410772dd6c4d37e267830b
Jeff Hann <jeff.hann@redspace.com>
2015-06-18 14:59:26 -0300

    - more changes to travis config trying to fix the automated build - adding oracle java sdk 7 to see if this works on both oracle and openjdk - adding redis since that is required

--- generate_keys.sh
+++ generate_keys.sh
@@ -7,5 +7,5 @@ openssl genrsa -out keys/FTDAuth.rsa 1024 -outform DER
 openssl pkcs8 -topk8 -nocrypt -in keys/FTDAuth.rsa -out keys/FTDAuth_pk8.rsa
 openssl rsa -in keys/FTDAuth.rsa -out keys/FTDAuth.pub -pubout
 
-ln -s $(PWD)/keys/FTDAuth_pk8.rsa /usr/local/keys/FTDAuth_pk8.rsa
-ln -s $(PWD)/keys/FTDAuth_pk8.pub /usr/local/keys/FTDAuth.pub
+ln -s $(pwd)/keys/FTDAuth_pk8.rsa /usr/local/keys/FTDAuth_pk8.rsa
+ln -s $(pwd)/keys/FTDAuth_pk8.pub /usr/local/keys/FTDAuth.pub
